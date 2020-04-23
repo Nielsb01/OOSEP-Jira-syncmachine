b413e406c0c2473b7b36a5f7b71f1d86b83d506a
Nielsb01 <nielsb1101@gmail.com>
2020-04-23 15:37:47 +0200

    worklogs maken werkt
    nog geen unit tests

--- src/main/java/nl/avisi/TimeWriteResource.java
+++ src/main/java/nl/avisi/TimeWriteResource.java
@@ -3,7 +3,9 @@ package nl.avisi;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.UnirestInstance;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -11,7 +13,7 @@ import javax.ws.rs.Path;
 @Path("send")
 public class TimeWriteResource {
 
-    //hier verdere implementatie van ophalen en verwerken post frontend
+    //hier verdere implementatie van ophalen en verwerken post front-end
     @GET
     public void sync(){
         //mock data moet van post komen
@@ -28,20 +30,20 @@ public class TimeWriteResource {
      * Method creates a new worklog for a user by sending a post request to the Jira API,
      * the location of where the worklog should be created is specified by the issueKey.
      * the standard comment of the worklog will be "Logging from JavaSyncApp"
-     * 
+     *
      * @param issueKey Tag used to specify Jira url.
      * @param username for basicAuth.
      * @param password for basicAuth.
      * @param timeSpend total worked time on issueKey.
      */
-    private void addWorklog(String issueKey, String username, String password, String timeSpend) {
+    public void addWorklog(String issueKey, String username, String password, String timeSpend) {
         String urlBase = "http://127.0.0.1/rest/api/2/issue/";
         String urlEnd = "/worklog";
         String url = urlBase + issueKey + urlEnd;
 
         WorklogDTO worklogDTO = new WorklogDTO();
         worklogDTO.setTimeSpent(timeSpend);
-        //worklogDTO.setComment("new Java test other account");
+                worklogDTO.setComment("new unirest");
 
         HttpResponse<JsonNode> response = Unirest.post(url)
                 .basicAuth(username, password)
---
+++ src/test/java/nl/avisi/TimeWriteResourceTest.java
@@ -0,0 +1,27 @@
+package nl.avisi;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import org.junit.Test;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class TimeWriteResourceTest {
+    @Mock
+    private TimeWriteResource sut;
+
+    @Test
+    public void addWorklog() {
+        // Arrange
+
+        // Act
+        sut.sync();
+
+        //Assert
+        verify(sut).addWorklog("KVNNTES-1", "Nielsb111", "OOSEGenua", "29m");
+    }
+}
