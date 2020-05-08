010487b85797e6155238107af115b1be840b6780
Bart Kessels <development@bk-mail.com>
2020-05-08 10:09:08 +0200

    Add database class

--- src/main/java/nl/avisi/PropertyReaders/DatabaseProperties.java
+++ src/main/java/nl/avisi/PropertyReaders/DatabaseProperties.java
@@ -1,4 +1,67 @@
 package nl.avisi.PropertyReaders;
 
+import javax.inject.Inject;
+
 public class DatabaseProperties {
+
+    /**
+     * File which contains the properties for the
+     * database
+     */
+    private final static String propertiesFile = "database.properties";
+
+    /**
+     * Configuration property which contains the
+     * connection string
+     */
+    private final static String connectionPropertyName = "connection";
+
+    /**
+     * Configuration property which contains the
+     * driver class name
+     */
+    private final static String driverPropertyName = "driver";
+
+    /**
+     * Class to read the actual properties from
+     * the file
+     */
+    private PropertyReader propertyReader;
+
+    /**
+     * Inject the property reader dependency
+     *
+     * @param propertyReader the property reader
+     */
+    @Inject
+    public void setPropertyReader(PropertyReader propertyReader) {
+        this.propertyReader = propertyReader;
+    }
+
+    /**
+     * Load the properties file into the
+     * property reader
+     *
+     */
+    public void loadPropertyFile() {
+        propertyReader.loadPropertyFile(propertiesFile);
+    }
+
+    /**
+     * Get the connection string
+     *
+     * @return the connection string for the database
+     */
+    public String getConnectionString() {
+        return propertyReader.getProperty(connectionPropertyName);
+    }
+
+    /**
+     * Get the class the database driver requires
+     *
+     * @return the class name of the database driver
+     */
+    public String getDriverName() {
+        return propertyReader.getProperty(driverPropertyName);
+    }
 }
--- src/main/java/nl/avisi/datasource/Database.java
+++ src/main/java/nl/avisi/datasource/Database.java
@@ -1,4 +1,47 @@
 package nl.avisi.datasource;
 
+import nl.avisi.PropertyReaders.DatabaseProperties;
+
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+
+    /**
+     * The class which contains the database
+     * driver and connection information
+     */
+    private DatabaseProperties databaseProperties;
+
+    /**
+     * Inject the database properties dependency
+     *
+     * @param databaseProperties
+     */
+    @Inject
+    public void setDatabaseProperties(DatabaseProperties databaseProperties) {
+        this.databaseProperties = databaseProperties;
+    }
+
+    /**
+     * Open the database connection based on the database
+     * driver set in the properties file
+     *
+     * @return a new connection to the database
+     * @throws SQLException when the database driver isn't installed on the system
+     */
+    public Connection conect() throws SQLException {
+        final String driverName = databaseProperties.getDriverName();
+        final String connectionString = databaseProperties.getConnectionString();
+
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            System.err.printf("Unable to find database driver %s\n", e.getMessage());
+        }
+
+        return DriverManager.getConnection(connectionString);
+    }
 }
