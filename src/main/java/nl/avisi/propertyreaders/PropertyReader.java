309e799b412b3eefa436336538f70f5368bc2e40
Ruben <ruben_eppink@live.nl>
2020-05-11 11:52:05 +0200

    fixed propertyreader imports

--- src/main/java/nl/avisi/propertyreaders/PropertyReader.java
+++ src/main/java/nl/avisi/propertyreaders/PropertyReader.java
@@ -1,9 +1,9 @@
-package nl.avisi.propertyReaders;
+package nl.avisi.propertyreaders;
 
-import nl.avisi.propertyReaders.exceptions.EmptyPropertyException;
-import nl.avisi.propertyReaders.exceptions.PropertyFileNotFoundException;
-import nl.avisi.propertyReaders.exceptions.PropertyFileNotLoadedException;
-import nl.avisi.propertyReaders.exceptions.PropertyNotFoundException;
+import nl.avisi.propertyreaders.exceptions.EmptyPropertyException;
+import nl.avisi.propertyreaders.exceptions.PropertyFileNotFoundException;
+import nl.avisi.propertyreaders.exceptions.PropertyFileNotLoadedException;
+import nl.avisi.propertyreaders.exceptions.PropertyNotFoundException;
 
 import javax.ws.rs.InternalServerErrorException;
 import java.io.IOException;
