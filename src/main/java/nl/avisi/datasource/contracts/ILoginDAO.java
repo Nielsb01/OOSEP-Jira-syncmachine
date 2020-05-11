87906089517f4bb29fc68ef76ef864444733af2a
Ruben <ruben_eppink@live.nl>
2020-05-11 14:41:08 +0200

    login model logica toegevoegd

---
+++ src/main/java/nl/avisi/datasource/contracts/ILoginDAO.java
@@ -0,0 +1,9 @@
+
+
+package nl.avisi.datasource.contracts;
+
+import nl.avisi.dto.UserDTO;
+
+public interface ILoginDAO {
+    UserDTO getLoginInfo(String username);
+}
---
+++ src/main/java/nl/avisi/model/Login.java
@@ -0,0 +1,44 @@
+package nl.avisi.model;
+
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.inject.Inject;
+import javax.ws.rs.NotAuthorizedException;
+
+/**
+ * Handles all logic that has to do with
+ * logging in.
+ */
+public class Login {
+
+    /**
+     * Resource responsible for communicating
+     * with the database
+     */
+    private ILoginDAO loginDAO;
+
+    @Inject
+    public void setLoginDAO(ILoginDAO loginDAO) {
+        this.loginDAO = loginDAO;
+    }
+
+    /**
+     * Validates the login information given by the user. Retrieves the
+     * stored password that corresponds with the given username from the
+     * database and compares this to the given password.
+     *
+     * @param loginDTO contains the login information of the user
+     *                 needed to verify their identity
+     * @return the UserID the corresponds to the supplied login information
+     */
+    public int validateCredentials(LoginDTO loginDTO) {
+        UserDTO userDTO = loginDAO.getLoginInfo(loginDTO.getUsername());
+
+        if (DigestUtils.sha256Hex(loginDTO.getPassword()).equals(userDTO.getPassword())) {
+            return userDTO.getUserID();
+        } else {
+            throw new NotAuthorizedException("Either the username and/or password wasn't correct!");
+        }
+    }
+}
