4f36521bb32c2fc95a5ebdc7e2cbddba4429b50e
Nielsb01 <nielsb1101@gmail.com>
2020-05-15 12:07:31 +0200

    Update WorklogRequestDTO.java
    java doc geschreven na max zijn verzoek er om

--- src/main/java/nl/avisi/dto/WorklogRequestDTO.java
+++ src/main/java/nl/avisi/dto/WorklogRequestDTO.java
@@ -1,10 +1,27 @@
 package nl.avisi.dto;
 
 import java.util.List;
-
+/**
+ * Class contains the necessary information to
+ * request worklogs between two given dates from the origin server
+ */
 public class WorklogRequestDTO {
+
+    /**
+     * is the first date from where in between the worklogs will be requested.
+     * if the from date is 2020-04-20 all the worklogs created on or later then this date will be requested
+     */
     private String from;
+
+    /**
+     * is the second date from where in between the worklogs will be requested.
+     * if the from date is 2020-04-25 all the worklogs created between the from date and this date will be requested
+     */
     private String to;
+
+    /**
+     * is a list of workers for whom the worklogs have to be requested "JIRAUSER10000"
+     */
     private List<String> worker;
 
     public String getFrom() {
