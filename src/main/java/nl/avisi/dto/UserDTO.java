f2fb70a9598a1e4417701c10feb37a56ef4fc785
Nielsb01 <nielsb1101@gmail.com>
2020-05-15 13:37:36 +0200

    Java doc geschreven voor dto's om consistend te blijven

--- src/main/java/nl/avisi/dto/UserDTO.java
+++ src/main/java/nl/avisi/dto/UserDTO.java
@@ -1,8 +1,26 @@
 package nl.avisi.dto;
-
+/**
+ * Class contains the return value of the {@link nl.avisi.datasource.LoginDAO}
+ * used to validate the login credentials and return the userID to the front end so
+ * the user can use this id with every following request to the backend.
+ */
 public class UserDTO {
+
+    /**
+     * is the users unique id selected from the database by username
+     */
     private int userID;
+
+    /**
+     * is the users unique username
+     */
     private String username;
+
+    /**
+     * is the users password in a hashed form to assure privacy
+     * in the validation process the filled in password in the {@link LoginDTO}
+     * will be hashed and compared to this hashed password
+     */
     private String password;
 
     public int getUserID() {
