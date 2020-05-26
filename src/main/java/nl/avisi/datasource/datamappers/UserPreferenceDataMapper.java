cc1bf8724394dd25780e13867cd27698dfeeb338
Bart Kessels <development@bk-mail.com>
2020-05-26 14:04:57 +0200

    Add missing javadoc

--- src/main/java/nl/avisi/datasource/datamappers/UserPreferenceDataMapper.java
+++ src/main/java/nl/avisi/datasource/datamappers/UserPreferenceDataMapper.java
@@ -7,6 +7,15 @@ import java.sql.SQLException;
 
 public class UserPreferenceDataMapper implements IDataMapper<UserPreferenceDTO> {
 
+    /**
+     * Converts the resultset that is passed in to
+     * a UserPreferenceDTO
+     *
+     * @param resultSet Contains the data that was retrieved from the database
+     * @return UserPreferenceDTO containing the data from the resultset
+     * @throws SQLException When any of the interactions with the resultset
+     * go wrong
+     */
     @Override
     public UserPreferenceDTO toDTO(ResultSet resultSet) throws SQLException {
         resultSet.next();
