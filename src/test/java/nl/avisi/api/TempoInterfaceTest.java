2c2a9e20de1bff87d10faa00068b9064cf82c5f0
max1e <47426263+max1e@users.noreply.github.com>
2020-05-27 15:52:01 +0200

    op advies van bart unit tests opgesplitst

--- src/test/java/nl/avisi/api/TempoInterfaceTest.java
+++ src/test/java/nl/avisi/api/TempoInterfaceTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 
 public class TempoInterfaceTest {
     
@@ -30,8 +32,9 @@ public class TempoInterfaceTest {
         sut.setJiraSynchronisationProperties(mockedJirasynchronisationProperties);
     }
 
+
     @Test
-    void testRequestOriginWorklogReturnsPostResult() {
+    void testRequestOriginWorklogCallsPostWithRightParameters() {
         // Arrange
         String originUrl = "url";
         String tempo_request_url = "rest/tempo-timesheets/4/worklogs/search";
@@ -41,7 +44,26 @@ public class TempoInterfaceTest {
         HttpResponse<JsonNode> expected = Mockito.mock(HttpResponse.class);
 
         Mockito.when(mockedJirasynchronisationProperties.getOriginUrl()).thenReturn(originUrl);
-        Mockito.when(mockedIRequest.post(originUrl + tempo_request_url, requestBody)).thenReturn(expected);
+        Mockito.when(mockedIRequest.post(anyString(), any(WorklogRequestDTO.class))).thenReturn(expected);
+
+        // Act
+        sut.requestOriginJiraWorklogs(requestBody);
+
+        // Assert
+        Mockito.verify(mockedIRequest).post(originUrl + tempo_request_url, requestBody);
+    }
+
+    @Test
+    void testRequestOriginWorklogReturnsPostResult() {
+        // Arrange
+        String originUrl = "url";
+
+        WorklogRequestDTO requestBody = new WorklogRequestDTO();
+
+        HttpResponse<JsonNode> expected = Mockito.mock(HttpResponse.class);
+
+        Mockito.when(mockedJirasynchronisationProperties.getOriginUrl()).thenReturn(originUrl);
+        Mockito.when(mockedIRequest.post(anyString(), any(WorklogRequestDTO.class))).thenReturn(expected);
 
         // Act
         HttpResponse<JsonNode> actual = sut.requestOriginJiraWorklogs(requestBody);
@@ -51,7 +73,7 @@ public class TempoInterfaceTest {
     }
 
     @Test
-    void testCreateWorklogOnDestinationServerReturnsPostResult() {
+    void testCreateWorklogOnDestinationServerCallsPostWithRightParameters() {
         // Arrange
         String destinationUrl = "url";
         String tempo_create_url = "rest/tempo-timesheets/4/worklogs";
@@ -61,7 +83,26 @@ public class TempoInterfaceTest {
         HttpResponse<JsonNode> expected = Mockito.mock(HttpResponse.class);
 
         Mockito.when(mockedJirasynchronisationProperties.getDestinationUrl()).thenReturn(destinationUrl);
-        Mockito.when(mockedIRequest.post(destinationUrl + tempo_create_url, requestBody)).thenReturn(expected);
+        Mockito.when(mockedIRequest.post(anyString(), any(DestinationWorklogDTO.class))).thenReturn(expected);
+
+        // Act
+        sut.createWorklogOnDestinationServer(requestBody);
+
+        // Assert
+        Mockito.verify(mockedIRequest).post(destinationUrl + tempo_create_url, requestBody);
+    }
+
+    @Test
+    void testCreateWorklogOnDestinationServerReturnsPostResult() {
+        // Arrange
+        String destinationUrl = "url";
+
+        DestinationWorklogDTO requestBody = new DestinationWorklogDTO();
+
+        HttpResponse<JsonNode> expected = Mockito.mock(HttpResponse.class);
+
+        Mockito.when(mockedJirasynchronisationProperties.getDestinationUrl()).thenReturn(destinationUrl);
+        Mockito.when(mockedIRequest.post(anyString(), any(DestinationWorklogDTO.class))).thenReturn(expected);
 
         // Act
         HttpResponse<JsonNode> actual = sut.createWorklogOnDestinationServer(requestBody);
