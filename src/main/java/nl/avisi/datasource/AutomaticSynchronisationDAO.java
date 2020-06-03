bfef0edc7780a761c11741aa4f99a4f52d3f5a23
max1e <47426263+max1e@users.noreply.github.com>
2020-06-03 11:15:51 +0200

    autosync dao aangemaakt

---
+++ src/main/java/nl/avisi/datasource/AutomaticSynchronisationDAO.java
@@ -0,0 +1,56 @@
+package nl.avisi.datasource;
+
+import nl.avisi.datasource.database.Database;
+
+import javax.inject.Inject;
+import javax.ws.rs.InternalServerErrorException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AutomaticSynchronisationDAO {
+
+    private Database database;
+
+    @Inject
+    public void setDatabase(Database database) {
+        this.database = database;
+    }
+
+    private static final String GET_LAST_SYNCHRONISATION_DATE_SQL = "SELECT synchronisation_date FROM automatic_synchronisation ORDER BY synchronisation_date DESC LIMIT 1";
+    private static final String SET_LAST_SYNCHRONISATION_DAT_SQL = "INSERT INTO automatic_synchronisation (synchronisation_date) VALUES (?)";
+
+    public String getLastSynchronisationDate() {
+
+        String lastSynchronisationDate;
+
+        try (Connection connection = database.connect();
+             PreparedStatement stmt = connection.prepareStatement(GET_LAST_SYNCHRONISATION_DATE_SQL)) {
+
+            ResultSet resultSet = stmt.executeQuery();
+            resultSet.next();
+
+            lastSynchronisationDate = resultSet.getString("synchronisation_date");
+
+        } catch (SQLException e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
+
+        return lastSynchronisationDate;
+
+    }
+
+    public void setLastSynchronisationDate(String newLastSynchronisationDate) {
+
+        try (Connection connection = database.connect();
+             PreparedStatement stmt = connection.prepareStatement(SET_LAST_SYNCHRONISATION_DAT_SQL)) {
+            stmt.setString(1, newLastSynchronisationDate);
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new InternalServerErrorException(String.format("Error occurred while updating the auto synchronisation status: %s", e.getMessage()));
+        }
+
+    }
+}
