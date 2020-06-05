13e57bdddc14a0758edbe4ad2e18b35173f982c5
Nielsb01 <nielsb1101@gmail.com>
2020-06-05 13:22:54 +0200

    unittests

--- src/test/java/nl/avisi/datasource/DatabaseLoggerDAOTest.java
+++ src/test/java/nl/avisi/datasource/DatabaseLoggerDAOTest.java
@@ -2,20 +2,57 @@ package nl.avisi.datasource;
 
 import nl.avisi.datasource.database.Database;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.InternalServerErrorException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
 class DatabaseLoggerDAOTest {
 
+    public static final String CLASS_NAME = "class_name";
     private DatabaseLoggerDAO sut;
     private Database mockedDatabase;
+    private PreparedStatement mockedStatement;
+    private Exception mockedException;
 
     @BeforeEach
     void setUp() {
         sut = new DatabaseLoggerDAO();
         mockedDatabase = mock(Database.class);
+        mockedStatement = mock(PreparedStatement.class);
+        mockedException = mock(Exception.class);
 
         sut.setDatabase(mockedDatabase);
     }
 
+    @Test
+    void testInsertLogIntoDatabaseClosesConnection() throws Exception {
+        //Arrange
+
+        final Connection mockConnection = mock(Connection.class);
+        when(mockedDatabase.connect()).thenReturn(mockConnection);
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockedStatement);
+
+
+        //Act
+        sut.insertLogIntoDatabase(CLASS_NAME, mockedException);
+
+        //Assert
+        verify(mockConnection).close();
+    }
+
+    @Test
+    void testInsertLogIntoDatabaseThrowsInternalServerErrorExceptionWhenSQLExceptionIsThrown() throws Exception {
+        //Arrange
+        when(mockedDatabase.connect()).thenThrow(SQLException.class);
+
+        //Act & Assert
+        assertThrows(InternalServerErrorException.class, () -> sut.insertLogIntoDatabase(CLASS_NAME, mockedException));
+    }
 }
