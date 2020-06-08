2a90a14d5a02ffab7f9ad88f047fa38ed76ea33b
Ruben <ruben_eppink@live.nl>
2020-06-08 11:19:52 +0200

    synchronisedTime omgezet naar synchronisedData

--- src/main/java/nl/avisi/dto/SynchronisedDataDTO.java
+++ src/main/java/nl/avisi/dto/SynchronisedDataDTO.java
@@ -1,10 +1,12 @@
 package nl.avisi.dto;
 
 /**
- * Holds the amount of seconds that were successfully
- * and non successfully synchronised during a manual synchronisation
+ * Holds the amount of seconds and amount of worklogs
+ * that were successfully and non successfully synchronised
+ * during a manual synchronisation
  */
-public class SynchronisedTimeDTO {
+public class SynchronisedDataDTO {
+
     /**
      * Amount of seconds that were successfully synchronised
      */
@@ -14,6 +16,42 @@ public class SynchronisedTimeDTO {
      * Amount of seconds that were failed to be synchronised
      */
     private int totalFailedSynchronisedSeconds;
+
+    /**
+     * Amount of worklogs that were successfully synchronised
+     */
+    private int totalSynchronisedWorklogs;
+
+    /**
+     * Amount of worklogs that were failed to be synchronised
+     */
+    private int totalFailedSynchronisedWorklogs;
+
+    public SynchronisedDataDTO(int totalSynchronisedSeconds, int totalFailedSynchronisedSeconds, int totalSynchronisedWorklogs, int totalFailedSynchronisedWorklogs) {
+        this.totalSynchronisedSeconds = totalSynchronisedSeconds;
+        this.totalFailedSynchronisedSeconds = totalFailedSynchronisedSeconds;
+        this.totalSynchronisedWorklogs = totalSynchronisedWorklogs;
+        this.totalFailedSynchronisedWorklogs = totalFailedSynchronisedWorklogs;
+    }
+
+    public SynchronisedDataDTO() {
+    }
+
+    public int getTotalSynchronisedWorklogs() {
+        return totalSynchronisedWorklogs;
+    }
+
+    public void setTotalSynchronisedWorklogs(int totalSynchronisedWorklogs) {
+        this.totalSynchronisedWorklogs = totalSynchronisedWorklogs;
+    }
+
+    public int getTotalFailedSynchronisedWorklogs() {
+        return totalFailedSynchronisedWorklogs;
+    }
+
+    public void setTotalFailedSynchronisedWorklogs(int totalFailedSynchronisedWorklogs) {
+        this.totalFailedSynchronisedWorklogs = totalFailedSynchronisedWorklogs;
+    }
 
     public int getTotalSynchronisedSeconds() {
         return totalSynchronisedSeconds;
